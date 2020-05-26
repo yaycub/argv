8e1655d304d8cd400a0826d4fb2bc07bcdc02c2f
Jacob Harrington <jharrington4@gmail.com>
2020-05-26 11:36:53 -0700

    adding note linked to mongodb and notes being added correctly

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-const argv = require('minimist')(process.argv.slice(2));
+require('dotenv').config();
+require('./lib/utils/connect')();
 
+const { parser, validator } = require('./lib/input');
+const execute = require('./lib/notes');
 
+const argv = process.argv.slice(2);
 
-console.log(argv);
+if(validator(argv)){
+  const action = parser(argv);
+  execute(action)
+    .then(newNote => console.log('note added:', {
+      note: newNote.text,
+      id: newNote._id
+    }));
+} else {
+  console.log('invalid action');
+}
